1eb75e24221cc0084a3ad55621a0c13c4e62bebc
Julian Ceipek <julian.ceipek@gmail.com>
2012-03-01 19:04:48 -0500

    More commenting; cleaning

--- update.py
+++ update.py
@@ -4,7 +4,7 @@ import coordinates
 import data_connections
 
 def do_update(username, currentplace = None, status=None):
-    signalStrengthDict = coordinates.getAvgCoords()
+    signalStrengthDict = coordinates.getAvgSignalStrengthDict()
     if not currCoords:
         pass # Failed to get coordinates
     else:
@@ -26,26 +26,29 @@ def do_update(username, currentplace = None, status=None):
         l = data_connections.unserializeMACData(result)
         return True, l, currentCoords
 
-def _getplatform():
+def __getPlatform():
+    '''
+    Return a standard readable string based on the operating system.
+    '''
     import sys
-    if sys.platform=='darwin':
+    if sys.platform.startswith('darwin'):
         return 'mac'
-    elif sys.platform=='win32':
+    elif sys.platform.startswith('win'):
         return 'win'
     elif sys.platform.startswith('linux'):
         return 'linux'
-    return ''
+    return sys.platform # non-standard platform
 
 def do_train(username, placename, mapx, mapy, mapw, data):
     strCoords = data_connections.serializeMACData(data)
     dictSend = {'username':username, 'placename':placename,'mapx':mapx,'mapy':mapy, 'mapw':mapw, 'data':strCoords}
-    dictSend['platform'] = _getplatform()
+    dictSend['platform'] = __getPlatform()
         
-    flag, result = data_connections.sendToServer('train.php', dictSend)
+    retStatus, result = data_connections.sendToServer('train.php', dictSend)
     print dictSend
-    if not flag:
+    if not retStatus:
         print 'Error:', result
-        return flag, result
+        return retStatus, result
         
     return True, result
 
@@ -77,5 +80,5 @@ def do_cloak(username):
 
 if __name__=='__main__':
     #~ do_update('bfisher','righthere')
-    print do_datapointexistance('AC30,out,rm308')
-    print do_datapointexistance('AC30,,out,rm308')
+    print do_datapointexistence('AC30,out,rm308')
+    print do_datapointexistence('AC30,,out,rm308')
