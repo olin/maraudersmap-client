661c4dd6b296a3b30047d1ec7ab7e5fc068fa095
Julian Ceipek <julian.ceipek@gmail.com>
2012-03-16 03:30:55 -0400

    Tried to add docs... again...

---
+++ marauders-map/api.py
@@ -0,0 +1,324 @@
+import urllib
+
+import webbrowser
+from configuration import Settings
+import signalStrength
+from getpass import getuser
+
+class Location(object):
+    """An object to represent the combination of a place name and a coordinate.
+
+    :param encodedName: The server-side representation of the name of the place. **Ex:** *WH,in,rm309*
+    :type encodedName: str
+    :param coordinate: The x,y,w coordinate of the place.
+    :type coordinate: :class:`Coordinate`
+    
+    """
+    def __init__(self, encodedName, coordinate):
+        self.coordinate = coordinate
+        self.encodedName = encodedName
+
+    def getReadableName(self):
+        """Parses the encodedName and converts it into a human-readable string.
+        This is useful for menu entries, for example.
+
+        :returns:  str -- the readable name
+        
+        .. warning::
+            This doesn't work properly; it is from the original code.
+        """
+        # location strings look like WH,in,rm309
+        location = self.encodedName.replace("OC", "MH") # Once upon a time, Milas hall was called Olin Center
+        # XXX: Should be changed on server, probably
+ 
+        try:
+            building, inside, description = location.split(",")
+        except:
+            return None
+
+        floor = building[2]
+        building = building[0:2]
+
+        if (inside == "in"):
+            inside = "inside"
+        elif (inside == "out"):
+            inside = "outside"
+        if (floor == "1"):
+            floor = "1st"
+        elif (floor == "2"):
+            floor = "2nd"
+        elif (floor == "3"):
+            floor = "3rd"
+        elif (floor == "4"):
+            floor = "4th"
+        elif (floor == "0"):
+            floor = "LL"
+
+
+        if (description.find("rm") != -1 or
+            description.find("room") != -1 or
+            description.find("Room") != -1 or
+            description.find("ROOM") != -1):
+            
+            # this has a room
+            description = description.replace("rm", "")
+            description = description.replace("room", "")
+            description = description.replace("Room", "")
+            description = description.replace("ROOM", "")
+
+            location = '%s %s%s' % (inside,building,description)
+        else:
+            if (floor != "LL"):
+                location = '%s %s %s floor%s' % (inside,building,floor,description)
+            else:
+                location = '%s %s (%s) %s' % (inside,building,floor,description)
+        
+        return location
+
+class Coordinate(object):
+    """An optimized way of representing an x,y,w location and (optionally) a distance.
+
+    :param x: x coordinate in pixels, on image w
+    :type x: int
+    :param y: y coordinate in pixels, on image w 
+    :type y: int
+    :param w: There are two images of maps in the original marauder's map. This attribute specifies to which map the x,y coordinates refer, 1 or 2. 
+    :type w: int
+
+    .. warning::
+        It is not possible to add attributes to this object
+    
+    """
+
+    __slots__ = ('x','y','w','distance')
+    def __init__(self, x, y, w, distance=0):
+        """
+
+        Args:
+            x: x coordinate, in px
+            y: y coordinate, in px
+            w: which image map the coordinate is for
+        Kwargs:
+            bar (str): Really, same as foo.
+        
+        """
+        self.x = x
+        self.y = y
+        self.w = w # Which map the coord refers to (1 or 2)
+        self.distance = distance
+
+# XXX: Sketchy global variable to get around server api requirement of getting location without posting it
+# XXX: Using this method until the server api gets fixed. I know it is REALLY BAD - Julian
+global lastSignalStrengthString
+lastSignalStrengthString = None 
+
+def openMap():
+    """
+    Opens the Marauder's Map user interface in the default web browser.
+    """
+    webbrowser.open(Settings.WEB_ADDRESS)
+
+def sendToServer(strPHPScript, dictParams):
+    """Uses urllib to send a dictionary to a PHP script on the server specified in configuration.py 
+
+    :returns: a tuple ``(successBOOL, responseSTR/explanation_of_failureSTR)``
+    """
+
+    # Construct url with the parameters specified in the dictionary
+    strUrl = "%s/%s?%s" % (Settings.SERVER_ADDRESS, strPHPScript, urllib.urlencode(dictParams))
+    print strUrl 
+    
+    try:
+        u = urllib.urlopen(strUrl)
+        ret = u.read().strip()
+        u.close()
+    except:
+        return False, 'Could not connect to server!'
+    
+    #See if successful
+    if not ret.startswith('success:'):
+        return False, ret
+    else:
+        return True, ret[len('success:'):]
+
+def __update(username, currPlaceName = None, status=None, refresh=True):
+    """Update can tell you where you are or tell the server where you are, without affecting
+    the database.
+
+    """
+
+    #XXX: Using this method until the server api gets fixed. I know it is REALLY BAD - Julian
+    global lastSignalStrengthString
+    if refresh or lastSignalStrengthString == None:
+        signalStrengthNodes = signalStrength.getAvgSignalNodes(samples=3, tsleep=0.15)
+        signalStrengthStr = ";".join([str(node) for node in signalStrengthNodes])
+        lastSignalStrengthString = signalStrengthStr
+    else:
+        signalStrengthStr = lastSignalStrengthString
+
+    dictSend = dict()
+    dictSend['username'] = username
+    dictSend['data'] = signalStrengthStr
+    dictSend['platform'] = __getPlatform()
+    
+    if currPlaceName:
+        dictSend['placename'] = currPlaceName  
+        
+    if status:
+        dictSend['status'] = status
+
+    flag, result = sendToServer('update.php', dictSend)
+    if not flag:
+        print 'Error:', result
+        return flag, result    
+    potentialLocations = list()
+    for point in result.split(';'):
+        if len(point) == 0: continue
+        # placename , distance , mapx, mapy, mapw            
+        point = point.split('|')
+        distanceFromPlace = float(point[1])        
+        x,y,w = int(point[2]), int(point[3]), int(point[4])
+        coord = Coordinate(x, y, w, distance=distanceFromPlace)
+        placeName = point[0]
+        potentialLocations.append(Location(placeName, coord))
+
+    return True, potentialLocations
+
+
+def getLocation():
+    """Get location from server.
+    
+    :returns: a list of potential :class:`Location`s, sorted from most to least likely.
+    """
+    
+    return __update(getuser())
+
+def postLocation(placeName):
+    """Post encoded location to server, without changing the database.
+    
+    Args:
+        placename (str): location to post to the server, in encoded server format
+
+    :returns: a list of potential :class:`Location`s, sorted from most to least likely.
+    """
+
+    print "Name to post:", placeName
+    
+    return __update(getuser(), currPlaceName=placeName)
+    
+def weakPostLocation(placeName):
+    '''
+    Post encoded location to server, without changing the database and without refreshing signal strength.
+    
+    Returns a list of potential locations, 
+    sorted from most to least likely.
+    '''
+
+    print "Name to post:", placeName
+    
+    return __update(getuser(), currPlaceName=placeName, refresh=False)
+
+def __getPlatform():
+    '''
+    Return a standard readable string based on the operating system.
+    '''
+    import sys
+    if sys.platform.startswith('darwin'):
+        return 'mac'
+    elif sys.platform.startswith('win'):
+        return 'win'
+    elif sys.platform.startswith('linux'):
+        return 'linux'
+    return sys.platform # non-standard platform
+
+def do_train(placename, coord):
+    #XXX: UNTESTED
+    '''
+    Tell server that a location in x,y,w space maps to a certain signal strength dictionary (data) and
+    encoded placename string.
+    This actually changes the database and can be used to create novel locations or improve the
+    database entry for existing locations.
+    '''
+
+    mapx, mapy, mapw = coord.x, coord.y, coord.w
+
+    signalStrengthNodes = signalStrength.getAvgSignalNodes(samples=3, tsleep=0.15)
+    signalStrengthStr = ";".join([str(node) for node in signalStrengthNodes])
+    
+    #XXX: Using this method until the server api gets fixed. I know it is REALLY BAD - Julian    
+    global lastSignalStrengthString
+    lastSignalStrengthString = signalStrengthStr
+
+    dictSend = {'username':getuser(), 'placename':placename,'mapx':mapx,'mapy':mapy, 'mapw':mapw, 'data':signalStrengthStr}
+    dictSend['platform'] = __getPlatform()
+        
+    retStatus, result = sendToServer('train.php', dictSend)
+    print dictSend
+    if not retStatus:
+        print 'Error:', result
+        return retStatus, result
+        
+    return True, result
+
+def unserializePersonData(serverResponseString):
+    '''
+    Takes in a pipe-separated string and 
+    outputs a dictionary with the keys
+    'username', 'placename', 'status', and 'lastupdate'
+    '''
+    personData = dict()
+    if serverResponseString == 'nobody': 
+        return 'nobody'
+    serverResponseArray = serverResponseString.split('|')
+    personData['username'] = serverResponseArray[0]
+    personData['placename'] = serverResponseArray[1]
+    personData['status'] = serverResponseArray[2]
+    personData['lastupdate'] = serverResponseArray[3]
+    return personData
+
+def do_query(username):
+    #XXX: UNTESTED
+    '''
+    Get a dictionary containing
+    username, placename, status, and lastupdate
+    of person with username
+    '''
+    flag, result = sendToServer('query.php', {'username':username})
+    if not flag:
+        print 'Error:', result
+        return flag, result
+    
+    result = unserializePersonData(result)
+    return True, result
+
+def do_datapointexistence(placename):
+    #XXX: UNTESTED
+    '''
+    Check if a point with a specific encoded placename exists
+    '''
+    flag, result = data_connections.sendToServer('pointexistence.php', {'placename':placename})
+    if not flag:
+        print 'Error:', result
+        return flag, result
+        
+    if result=='true': return True, True
+    else: return True, False
+
+def do_cloak(username):
+    #XXX: UNTESTED
+    '''
+    Tell the server to stop displaying a user with username on the map.
+
+    This is kind of sketchy and probably shoudn't be possible?
+    '''
+    flag, result = sendToServer('cloak.php', {'username':username})
+    if not flag:
+        print 'Error:', result
+        return flag, result
+    
+    return flag, result
+
+if __name__=='__main__':
+    #~ do_update('bfisher','righthere')
+    print do_datapointexistence('AC30,out,rm308')
+    print do_datapointexistence('AC30,,out,rm308')
---
+++ marauders-map/mapclient.py
@@ -0,0 +1,316 @@
+#!/usr/bin/python
+
+# Main Marauder's Map Application
+# Consists of an icon in the system tray with a menu
+# and a preferences window that is accessed through the Preferences... entry
+
+# Program structure:
+#  The preferencesWindow is the root node of the program and hides itself by default
+#  It is used for infrequent configuration changes
+#  The systemTray ties in to all of the external functions required for the map
+
+from PySide import QtCore
+from PySide import QtGui
+
+import api
+
+class GeneralPrefs(QtGui.QWidget):
+    def __init__(self):
+        super(GeneralPrefs, self).__init__()
+        mainLayout = QtGui.QVBoxLayout()
+        mainLayout.addWidget(QtGui.QLabel("You will be able to configure basic stuff here."))
+        self.setLayout(mainLayout)
+
+class AdvancedPrefs(QtGui.QWidget):
+    def __init__(self):
+        super(AdvancedPrefs, self).__init__()
+        mainLayout = QtGui.QVBoxLayout()
+        mainLayout.addWidget(QtGui.QLabel("You will be able to configure advanced stuff here."))
+        self.setLayout(mainLayout)
+
+class LocationWorker(QtCore.QObject):
+    #TODO: Implement some kind of queue of tasks to prevent freezing/long shutdown time
+    # A signal sent by the LocationWorker whenever the location is reported by the server
+    locationUpdatedSignal = QtCore.Signal(list)
+
+    def __init__(self):
+        super(LocationWorker, self).__init__()
+        self.canWork = True        
+
+    @QtCore.Slot()
+    def getLocation(self):
+        if self.canWork:
+            print "Getting location"
+            responseTuple = api.getLocation()
+            self.locationUpdatedSignal.emit(responseTuple)
+            flag, response = responseTuple
+            if flag:
+                locations = response
+                print locations
+                api.weakPostLocation(locations[0].encodedName)
+        else:
+            print "Location getting/posting disabled"
+
+    @QtCore.Slot(str)
+    def postLocation(self, loc):  
+        # Should only be invoked directly by a user specifying the correct location    
+        if self.canWork:        
+            print "Posting User-Specified Location:", loc.getReadableName()
+            api.do_train(loc.encodedName, loc.coordinate)
+            api.weakPostLocation(loc.encodedName)
+
+    @QtCore.Slot()
+    def stopWorking(self):
+        # Called when user goes offline (or program about to shut down?)
+        self.canWork = False
+
+class PreferencesWindow(QtGui.QDialog):
+    
+    # A signal that can be sent to the LocationWorker to tell it
+    # to get the user's new location    
+    updateSignal = QtCore.Signal()
+    
+    offlineSignal = QtCore.Signal()
+
+    postSignal = QtCore.Signal(api.Location)
+
+    def __init__(self):
+        super(PreferencesWindow, self).__init__()
+        self.setup()
+
+    def setup(self):
+        mainLayout = QtGui.QVBoxLayout()
+        mainLayout.addWidget(self.createTabPane())
+        self.setLayout(mainLayout)
+        
+        self.setSize(500,200)
+
+        self.setWindowTitle("Marauder's Map @ Olin Preferences") 
+
+        self.createSystemTray()
+        self.setupBackgroundThread()
+        self.startRefreshTimer()
+
+    def setSize(self, width, height):
+        self.setMinimumWidth(width)        
+        self.setMaximumWidth(width)                
+        self.setMinimumHeight(height)
+        self.setMaximumHeight(height)
+
+    def createTabPane(self):
+        '''
+        Create and return a tab pane with the options
+        General, Advanced
+
+        This pane is like the one used in the Mac OS X preferences dialog
+        '''
+        tabPane = QtGui.QTabWidget()
+        tabPane.addTab(GeneralPrefs(), "General")
+        tabPane.addTab(AdvancedPrefs(), "Advanced")
+        return tabPane
+
+    def createSystemTray(self):
+        '''
+        
+        '''
+        self.sysTrayIconDefault = QtGui.QIcon("demoIcon.png")
+        self.sysTrayIconClicked = QtGui.QIcon("demoIconWhite.png")        
+        self.sysTray = QtGui.QSystemTrayIcon(self, icon=self.sysTrayIconDefault)
+        self.sysTray.setToolTip("Marauder's Map")
+
+        self.sysTray.activated.connect(self.sysTrayMenuClicked)
+        self.createSystemTrayActions()
+        self.sysTrayMenu = self.createSystemTrayMenu()
+        self.sysTray.setContextMenu(self.sysTrayMenu)
+        self.sysTrayMenu.aboutToHide.connect(self.sysTrayMenuClosed) # XXX: NEVER GETS TRIGGERED ON MAC OS X!?!
+        # I expected this to emit on menu close when no action is selected
+
+        self.sysTray.show()
+
+    def createSystemTrayActions(self):
+        self.openAction = QtGui.QAction("&Open Map", self, triggered=api.openMap)
+        self.refreshAction = QtGui.QAction("&Refresh My Location", self, triggered=self.sysTrayInitiateLocationRefresh)
+        self.locationIndicator = QtGui.QAction("Location: Unknown", self, enabled=False)
+        self.correctLocationAction = QtGui.QAction("&Correct My Location", self, enabled=False)
+        self.otherLocationAction = QtGui.QAction("Other...", self)
+        self.offlineAction = QtGui.QAction("&Go Offline", self, triggered=self.sysTrayGoOffline)
+        self.prefsAction = QtGui.QAction("&Preferences...", self, triggered=self.display)
+        self.quitAction = QtGui.QAction("&Quit Marauder's Map", self, triggered=self.sysTrayQuitAction)
+
+    def createSystemTrayMenu(self):
+        sysTrayMenu = QtGui.QMenu(self)
+        sysTrayMenu.addAction(self.openAction)
+        
+        sysTrayMenu.addSeparator()
+        sysTrayMenu.addAction(self.refreshAction)
+        sysTrayMenu.addSeparator()
+        sysTrayMenu.addAction(self.locationIndicator)
+        sysTrayMenu.addAction(self.correctLocationAction)        
+        sysTrayMenu.addSeparator()
+        sysTrayMenu.addAction(self.offlineAction)
+        sysTrayMenu.addAction(self.prefsAction)
+        sysTrayMenu.addSeparator()
+        sysTrayMenu.addAction(self.quitAction)
+        
+        return sysTrayMenu
+        
+    def startRefreshTimer(self):
+        self.bgThread.start()        
+        self.refreshTimer = QtCore.QTimer(self)
+        self.refreshTimer.timeout.connect(self.refreshLocation)
+        self.refreshTimer.start(10000)
+
+    def refreshLocation(self):
+        '''
+        Sends a signal to the LocationWorker in bgThread
+        to get the location
+        '''
+        self.sysTray.showMessage("Updating", "Determining Location...")
+        self.updateSignal.emit()
+
+    def postLocation(self, loc):
+        '''
+        Sends a signal to the LocationWorker in bgThread
+        to post a specified location
+        '''
+        self.postSignal.emit(loc)
+        self.locationIndicator.setText(loc.getReadableName())
+
+    def setupBackgroundThread(self):
+        self.bgThread = QtCore.QThread()
+        self.locationWorker = LocationWorker()
+        self.locationWorker.locationUpdatedSignal.connect(self.locationSlot)
+        self.updateSignal.connect(self.locationWorker.getLocation)
+        self.postSignal.connect(self.locationWorker.postLocation)
+        self.offlineSignal.connect(self.locationWorker.stopWorking)
+        self.locationWorker.moveToThread(self.bgThread)
+
+    def display(self):
+        '''
+        Display the preferences window
+        '''
+        # TODO: There is an ugly jumping effect where the window starts out below
+        #  the active application and moves to the top. I'd like it to appear on top
+        self.show()
+        self.raise_()
+
+    def closeEvent(self, event):
+        '''
+        When the close button is pressed on the preferences window,
+        hide it; don't close or minimize it.
+
+        Note: Don't call this function manually!
+        '''
+        self.hide()
+        event.ignore()
+
+    # System Tray Actions:
+    @QtCore.Slot(QtGui.QSystemTrayIcon.ActivationReason)
+    def sysTrayMenuClicked(self, reason):
+        '''
+        Connected to the 'activated' signal of the system tray.
+        Changes the icon to look good when clicked
+        '''
+        if reason == QtGui.QSystemTrayIcon.ActivationReason.Trigger:
+            #Single Click to open menu
+            self.sysTray.setIcon(self.sysTrayIconClicked)
+        # NOTE: Below commented because unused. Can use later if we want
+        #elif reason == QtGui.QSystemTrayIcon.ActivationReason.DoubleClick:
+        #    # Double click
+        #    pass
+
+    @QtCore.Slot()
+    def sysTrayMenuClosed(self):  
+        print "Closed Menu"
+        # XXX: NEVER GETS TRIGGERED on Mac OsX
+        self.sysTray.setIcon(self.sysTrayIconDefault)
+
+    def sysTrayQuitAction(self):
+        '''
+        Cleans up and quits the application.
+        '''
+
+        self.offlineSignal.emit()        
+        self.bgThread.quit()
+        while not self.bgThread.isFinished():
+            continue # Wait until thread done 
+        # On Ubuntu 10.10 (at least), a Python fatal error is encountered if the
+        # window is not hidden before the application exits
+        self.hide()
+        QtGui.qApp.quit()
+
+    def sysTrayInitiateLocationRefresh(self):
+        self.refreshLocation()
+
+    def sysTrayGoOffline(self):
+        self.offlineSignal.emit()
+
+    # Background actions
+    @QtCore.Slot(tuple)
+    def locationSlot(self, flagResponseTuple):
+        '''
+        Slot that gets data whenever a location refresh
+        happens
+        '''
+        flag, response = flagResponseTuple
+        if flag:
+            potLocs = response
+            subMenu = QtGui.QMenu("Correct Location Submenu", self)
+            for potLoc in potLocs:
+                def correctLocation(realLoc):
+                    def postFunction():
+                        self.postLocation(realLoc)
+                    return postFunction
+                
+                subAction = QtGui.QAction(potLoc.getReadableName(), self, triggered=correctLocation(potLoc))
+                subMenu.addAction(subAction)
+            subMenu.addSeparator()            
+            subMenu.addAction(self.otherLocationAction)
+            self.correctLocationAction.setMenu(subMenu)
+            self.correctLocationAction.setEnabled(True)
+
+            self.mostLikelyLoc = potLocs[0]
+            self.locationIndicator.setText(self.mostLikelyLoc.getReadableName())
+
+            self.sysTray.showMessage("Location: %s" % self.mostLikelyLoc.getReadableName(), "Click here to fix the location.")
+
+        else:
+            self.locationIndicator.setText("Unable to Connect to Server")
+            self.correctLocationAction.setEnabled(False)
+            
+def setupWindow():
+    '''
+    Create and return the preferences window,
+    which owns every other element
+    '''
+    QtGui.QApplication.setQuitOnLastWindowClosed(False)
+    preferencesWindow = PreferencesWindow()
+    #preferencesWindow.display()
+    return preferencesWindow
+
+def canLaunch():
+    '''
+    Checks if the application is unable to launch because of missing
+    system features.
+
+    Returns CanLaunch_BOOL, Reason_STR
+    '''
+    if not QtGui.QSystemTrayIcon.isSystemTrayAvailable():
+        return False, "Failed to detect presence of system tray"
+    else:
+        return True, None
+
+if __name__ == '__main__':
+    import sys
+    
+    app = QtGui.QApplication(sys.argv)
+    ableToLaunch, reason = canLaunch()
+    
+    if not ableToLaunch:
+        print "ERROR: Unable to launch Marauder's Map!"
+        print reason
+        sys.exit(1)
+    else:
+        # Note: we have to retain a reference to the window so that it isn't killed
+        preferencesWindow = setupWindow()
+        sys.exit(app.exec_())
