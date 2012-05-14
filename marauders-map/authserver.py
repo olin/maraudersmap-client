53c662ba17de10ed1ab2ea296e11d13b5484500b
Tim Cameron Ryan <id@timryan.org>
2012-05-14 02:35:57 -0400

    Clean up authserver

--- marauders-map/authserver.py
+++ marauders-map/authserver.py
@@ -4,8 +4,11 @@ import os
 import cgi
 import sys
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+
+def auth_callback(cookies_dict):
+	print(cookies_dict)
  
-class customHTTPServer(BaseHTTPRequestHandler):
+class MapAuthHTTPServer(BaseHTTPRequestHandler):
 	def do_GET(self):
 		self.send_response(200)
 		self.send_header('Content-type', 'text/html')
@@ -22,18 +25,25 @@ class customHTTPServer(BaseHTTPRequestHandler):
 	def do_POST(self):
 		length = int(self.headers.getheader('content-length'))
 		postvars = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
-		print(postvars.get('browserid', ''))
-		print(postvars.get('session', ''))
+
 
 		self.send_response(303)
 		self.send_header('Location', 'http://map.fwol.in/')
 		self.end_headers()
 		self.wfile.write('User authenticated. Redirecting to the map.')
 
+		auth_callback({
+			"browserid": postvars.get('browserid', ''),
+			"session": postvars.get('session', '')
+		})
+
 def main():
 	try:
-		server = HTTPServer(('', 0),customHTTPServer)
-		print 'server started: http://localhost:' + str(server.server_port) + '/'
+		server = HTTPServer(('', 0), MapAuthHTTPServer)
+		# use this port number for other things
+		port = server.server_port
+
+		print 'server started: http://localhost:' + str(port) + '/'
 		server.serve_forever()
 	except KeyboardInterrupt:
 		server.socket.close() 
