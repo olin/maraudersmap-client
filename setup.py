4e5777243130234cea8dcb5e3a0582497c8050d1
Julian Ceipek <julian.ceipek@gmail.com>
2012-05-15 18:41:11 -0400

    Improved mac build script

--- setup.py
+++ setup.py
@@ -15,14 +15,14 @@ import sys
 
 if sys.platform.startswith('darwin'):
     from setuptools import setup
-    APP = ['maraudersmap/mapclient.py']
-    DATA_FILES = ['maraudersmap/data/qt.conf', # Used to fix mysterious segfault based on advice from http://www.thetoryparty.com/2009/03/03/pyqt4-i-hate-you/
-                  'maraudersmap/data/demoIcon.png',
-                  'maraudersmap/data/demoIconWhite.png']
+    APP = ['src/mapclient.py']
+    DATA_FILES = ['src/data/qt.conf', # Used to fix mysterious segfault based on advice from http://www.thetoryparty.com/2009/03/03/pyqt4-i-hate-you/
+                  'src/data/demoIcon.png',
+                  'src/data/demoIconWhite.png']
 
     # The plist file specifies that there should be no icon in the dock
     # See: http://www.macosxtips.co.uk/index_files/disable-the-dock-icon-for-any-application.php
-    OPTIONS = {'argv_emulation': True, 'plist':'maraudersmap/data/Info.plist'}
+    OPTIONS = {'argv_emulation': True, 'plist':'src/data/Info.plist'}
 
     setup(
         app=APP,
@@ -30,7 +30,7 @@ if sys.platform.startswith('darwin'):
         options={'py2app': OPTIONS},
         setup_requires=['py2app'],
         packages=['mapclient'],
-        package_dir={'mapclient': 'maraudersmap'},
+        package_dir={'mapclient': 'src'},
         package_data={'mapclient': ['data/*']}
     )
 elif sys.platform.startswith('linux'):
